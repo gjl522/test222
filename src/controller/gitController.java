1aa29524e63acedd55edd6d0c73b9183d06e535c
DELL <986174990@qq.com>
2020-10-22 11:29:19 +0800

    第一个分支

--- src/controller/gitController.java
+++ src/controller/gitController.java
@@ -3,6 +3,16 @@ package controller;
 public class gitController {
     public static void main(String[] args) {
         System.out.println("这是git测试的控制器");
+
+        System.out.println("这是修改过之后的分支代码");
+
+        System.out.println();
+        //
+
+    }
+    public static String getNum(){
+
+        return null;
     }
 
 
